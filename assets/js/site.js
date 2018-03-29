1eb10663ad9066942cb0f8ddcc5daf40625450f1
Guido Flohr <guido.flohr@cantanea.com>
2018-03-29 21:31:59 +0300

    typo

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,6 +5,6 @@ var html = document.getElementsByTagName('html').item(0);
 if (html) {
     html.className = html.className.replace(/^no-js$/, '');
 
-    // Of if support for really old browsers and IE9 is not an issue:
+    // Or if support for really old browsers and IE9 is not an issue:
     //html.classList.remove('no-js');
 }
